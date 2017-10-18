d40bac26f62a57eddb8c893e01828ebc90ae63a9
Ikaro Lopes <ikaropinheiro6@gmail.com>
2017-10-18 16:24:28 -0200

    Aprendendo a criar Controller e View

---
+++ application/controllers/Olamundo.php
@@ -0,0 +1,17 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Olamundo extends CI_Controller {
+
+	public function index()
+	{
+		$dados['mensagem'] = "Ola Mundo!";
+		$this->load->view('olamundo', $dados);
+	}
+
+	public function teste()
+	{
+		$dados['mensagem'] = 'Testando';
+		$this->load->view('olamundo', $dados);
+	}
+}
