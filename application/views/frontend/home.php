f2fce8cdba02525bfb9000520aeca6e712e8d142
Ikaro Lopes <ikaropinheiro6@gmail.com>
2017-10-20 17:25:34 -0200

    Selecionando o nome do usuário feito por uma relação de tabelas

--- application/views/frontend/home.php
+++ application/views/frontend/home.php
@@ -19,7 +19,7 @@
                     <a href="<?php echo base_url('postagem/'.$postagem->id.'/'. limpar($postagem->titulo) ) ?>"><?php echo $postagem->titulo ?></a>
                 </h2>
                 <p class="lead">
-                    por <a href="index.php">Um mano por ai</a>
+                    por <a href="<?php echo base_url('autor/'.$postagem->idautor.'/'. limpar($postagem->nome)) ?>"><?php echo $postagem->nome ?></a>
                 </p>
                 <p><span class="glyphicon glyphicon-time"></span><?php echo postadoem($postagem->data) ?></p>
                 <hr>
