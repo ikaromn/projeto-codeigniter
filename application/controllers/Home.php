feaa4f4b74ace003a280f88bd8738adbaea790f0
Ikaro Lopes <ikaropinheiro6@gmail.com>
2017-10-20 13:19:00 -0200

    Adicionando os posts em destaques na minha view de Home

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -13,6 +13,8 @@ class Home extends CI_Controller {
 	public function index()
 	{
 		$dados['categorias'] = $this->categorias;
+		$this->load->model('publicacoes_model', 'modelpublicacoes');
+		$dados['postagens'] = $this->modelpublicacoes->destaques_home();
 		$this->load->view('frontend/template/html-header', $dados);
 		$this->load->view('frontend/template/header');
 		$this->load->view('frontend/home');
--- application/views/frontend/home.php
+++ application/views/frontend/home.php
@@ -11,52 +11,25 @@
                     <small>Postagens Recentes</small>
                 </h1>
 
-                <!-- First Blog Post -->
-                <h2>
-                    <a href="#">Blog Post Title</a>
-                </h2>
-                <p class="lead">
-                    por <a href="index.php">Start Bootstrap</a>
-                </p>
-                <p><span class="glyphicon glyphicon-time"></span> Postado em 25 de Janeiro de 2017 10:00</p>
-                <hr>
-                <img class="img-responsive" src="http://placehold.it/900x300" alt="">
-                <hr>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, veritatis, tempora, necessitatibus inventore nisi quam quia repellat ut tempore laborum possimus eum dicta id animi corrupti debitis ipsum officiis rerum.</p>
-                <a class="btn btn-primary" href="#">Leia mais <span class="glyphicon glyphicon-chevron-right"></span></a>
-
-                <hr>
-
-                <!-- Second Blog Post -->
-                <h2>
-                    <a href="#">Blog Post Title</a>
-                </h2>
-                <p class="lead">
-                    por <a href="index.php">Start Bootstrap</a>
-                </p>
-                <p><span class="glyphicon glyphicon-time"></span> Postado em 25 de Janeiro de 2017 10:00</p>
-                <hr>
-                <img class="img-responsive" src="http://placehold.it/900x300" alt="">
-                <hr>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam, quasi, fugiat, asperiores harum voluptatum tenetur a possimus nesciunt quod accusamus saepe tempora ipsam distinctio minima dolorum perferendis labore impedit voluptates!</p>
-                <a class="btn btn-primary" href="#">Leia mais <span class="glyphicon glyphicon-chevron-right"></span></a>
-
-                <hr>
-
                 <!-- Third Blog Post -->
+                <?php
+                    foreach($postagens as $postagem){
+                ?>
                 <h2>
-                    <a href="#">Blog Post Title</a>
+                    <a href="<?php echo base_url('postagem/'.$postagem->id.'/'. limpar($postagem->titulo) ) ?>"><?php echo $postagem->titulo ?></a>
                 </h2>
                 <p class="lead">
-                    por <a href="index.php">Start Bootstrap</a>
+                    por <a href="index.php">Um mano por ai</a>
                 </p>
                 <p><span class="glyphicon glyphicon-time"></span> Postado em 25 de Janeiro de 2017 10:00</p>
                 <hr>
                 <img class="img-responsive" src="http://placehold.it/900x300" alt="">
                 <hr>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, voluptates, voluptas dolore ipsam cumque quam veniam accusantium laudantium adipisci architecto itaque dicta aperiam maiores provident id incidunt autem. Magni, ratione.</p>
-                <a class="btn btn-primary" href="#">Leia mais <span class="glyphicon glyphicon-chevron-right"></span></a>
+                <p><?php echo $postagem->subtitulo ?></p>
+                <a class="btn btn-primary" href="<?php echo base_url('postagem/'.$postagem->id.'/'. limpar($postagem->titulo) ) ?>">Leia mais <span class="glyphicon glyphicon-chevron-right"></span></a>
 
                 <hr>
-
+                <?php
+                    }
+                ?>
             </div>
