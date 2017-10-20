f9081585c61838a0c1cf9c34a6613b625a4212c0
Ikaro Lopes <ikaropinheiro6@gmail.com>
2017-10-20 15:39:11 -0200

    Adicionando função helper para alterar o modo no qual a data é exibida

--- application/views/frontend/home.php
+++ application/views/frontend/home.php
@@ -21,7 +21,7 @@
                 <p class="lead">
                     por <a href="index.php">Um mano por ai</a>
                 </p>
-                <p><span class="glyphicon glyphicon-time"></span> Postado em 25 de Janeiro de 2017 10:00</p>
+                <p><span class="glyphicon glyphicon-time"></span><?php echo postadoem($postagem->data) ?></p>
                 <hr>
                 <img class="img-responsive" src="http://placehold.it/900x300" alt="">
                 <hr>
