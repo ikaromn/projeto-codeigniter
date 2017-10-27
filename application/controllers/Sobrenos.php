432d7968e6d75f3c5f754e26ad3b7eb7dc9ff7a4
Ikaro Lopes <ikaropinheiro6@gmail.com>
2017-10-27 15:51:36 -0200

    Adicionando view/controller e função na model usuário para a inserção da página Sobre nós

--- application/controllers/Sobrenos.php
+++ application/controllers/Sobrenos.php
@@ -8,22 +8,22 @@ class Sobrenos extends CI_Controller
         parent::__construct();
 		$this->load->model('categorias_model', 'modelcategorias');
 		$this->categorias = $this->modelcategorias->listar_categorias();
+		$this->load->model('usuarios_model', 'modelusuarios');
     }
 
-    public function index($id, $slug=null)
+    public function index()
 	{
 		$dados['categorias'] = $this->categorias;
-		$this->load->model('publicacoes_model', 'modelpublicacoes');
-		$dados['postagens'] = $this->modelpublicacoes->categoria_pub($id);
+		$dados['autores'] = $this->modelusuarios->listar_autores();
+
 
 		//dados a serem enviados para o Cabeçalho
-		$dados['titulo'] = 'Categorias';
-        $dados['subtitulo'] = '';
-        $dados['subtitulodb'] = $this->modelcategorias->listar_titulo($id);
+		$dados['titulo'] = 'Sobre nós';
+        $dados['subtitulo'] = 'Conheça nossa equipe';
         
 		$this->load->view('frontend/template/html-header', $dados);
 		$this->load->view('frontend/template/header');
-		$this->load->view('frontend/categoria');
+		$this->load->view('frontend/sobrenos');
 		$this->load->view('frontend/template/aside');
 		$this->load->view('frontend/template/footer');
 		$this->load->view('frontend/template/html-footer');
@@ -32,7 +32,6 @@ class Sobrenos extends CI_Controller
 	public function autores($id, $slug=null)
 	{
 		$dados['categorias'] = $this->categorias;
-		$this->load->model('usuarios_model', 'modelusuarios');
 		$dados['autores'] = $this->modelusuarios->listar_autor($id);
 
 		//dados a serem enviados para o Cabeçalho
